1c60ae54187cdc6cc1ce26b78261ec44096f8a56
vnshanmukh <venkatanagashanmukh.chava@slu.edu>
2022-10-03 18:39:59 -0500

    BLD-4 connecting to database and creating the userprofile table

---
+++ Backend/database/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from database.connection import Base
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+class User(Base):
+    __tablename__ = 'userprofile'
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String,nullable= False, unique= True)
+    password = Column(String,nullable= False)
+    firstname = Column(String,nullable= False)
+    lastname = Column(String,nullable= False)
+    created_at = Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from database import models
+from database.connection import engine
 import routers.signup
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine) #creates the tables from models.py
+
 app.include_router(routers.signup.router)
