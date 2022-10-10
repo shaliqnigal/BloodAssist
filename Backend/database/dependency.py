15c1e0f26c4b2ff498effba6667234c7e1399db5
vnshanmukh <venkatanagashanmukh.chava@slu.edu>
2022-10-09 20:52:44 -0500

    BLD-21 added dependency injection and testing

---
+++ Backend/database/dependency.py
@@ -0,0 +1,7 @@
+from symbol import import_stmt
+from sqlalchemy.orm import Session
+from database.connection import engine
+
+def dataBase():
+    with Session(engine) as session:
+        yield session
--- Backend/routers/signup.py
+++ Backend/routers/signup.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter,HTTPException, status
 from database import schemas
 from database.crud import userCRUD
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from database.dependency import dataBase
 
 router = APIRouter()
 
 @router.post("/signup")
-async def create_user(userDetails :schemas.UserCreate):
-    if userCRUD.search_user_email(userDetails.email):
+async def create_user(userDetails :schemas.UserCreate, session: Session = Depends(dataBase)):
+    if userCRUD.search_user_email(userDetails.email, session):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"User with {userDetails.email} already exists")
     else:
-        userCRUD.user_create(userDetails)
+        userCRUD.user_create(userDetails, session)
     return {"User registered Successfully"}
