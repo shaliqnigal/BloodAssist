837b1c095b41028e30802ef347ec8da20b3f15c8
hkalkoda <harikrishna.kalkoda@slu.edu>
2022-10-24 15:10:24 -0500

    BLD-29 added register router to store data of donor and changes in styles for bg image

--- Backend/routers/donor.py
+++ Backend/routers/donor.py
@@ -1,9 +1,13 @@
+from database import models,oauth,schemas
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 from database.dependency import dataBase
 
 router = APIRouter()
 @router.post('/register_donor')
-async def create_donor(donor:schemas.Donor,db: Session = Depends(dataBase)):
-    
-    return {"donor path operation"}
+async def create_donor(donor:schemas.Donor,db: Session = Depends(dataBase), current_user :  int  = Depends(oauth.get_current_user)):
+    new_donor = models.Donor(owner_id = current_user, **donor.dict())
+    db.add(new_donor)
+    db.commit()
+    db.refresh(new_donor)
+    return new_donor
